ebd5ca1df3005eb57469b596cd695cabebfbf1fc
SUBHANKAR MANNA <mannasubhankar06@gmail.com>
2025-08-19 23:35:24 +0530

    Adding python learning chapter-4

---
+++ Chapter-4/Practice/program-1.py
@@ -0,0 +1,21 @@
+# write a program to store some fruits in a list entered by the user
+
+fruits = []
+
+fruit1 = input("Enter the fruit you like:")
+fruits.append(fruit1)
+
+fruit2 = input("Enter the fruit you like:")
+fruits.append(fruit2)
+
+fruit3 = input("Enter the fruit you like:")
+fruits.append(fruit3)
+
+fruit4 = input("Enter the fruit you like:")
+fruits.append(fruit4)
+
+fruit5 = input("Enter the fruit you like:")
+fruits.append(fruit5)
+
+
+print("The fruits you like are:", fruits)
---
+++ Chapter-4/list.py
@@ -0,0 +1,6 @@
+# Learning about lists
+# unlike strings, lists are mutable
+list = ['apple', 'banana', 'cherry', True, 123, None]
+
+print (list)
+print(list[0:4]) # prints the first four elements of the list
