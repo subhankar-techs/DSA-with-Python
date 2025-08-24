0f3e5b230bc11268419f670bf3dcca31eb9a46de
SUBHANKAR <mannasubhankar06@gmail.com>
2025-08-24 23:05:50 +0530

    Set and Dictionary Learning

---
+++ Chapter-5/dict-2.py
@@ -0,0 +1,7 @@
+marks = {
+    "Amit": 100,
+    "Shubham": 55,
+    "Subhankar": 99
+}
+print(marks)
+print(marks["Amit"])
---
+++ Chapter-5/sets-3.py
@@ -0,0 +1,3 @@
+d = {}  #To create Empty Dictionary
+
+emptySet = set() # To create empty set we use set()
